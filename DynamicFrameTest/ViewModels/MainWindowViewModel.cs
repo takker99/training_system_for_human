fd8cd1cc5f0fc2ea9bdca7ed979851edbafd268c
takker99 <electronic.homuhomu@gmail.com>
2020-05-14 20:39:31 +0900

    :sparkles: :alembic: Load a Flow document, register an event handler and get a property dynamically
    動的に
    - xaml読み込み
    - event handler登録
    - propertyの取得
    ができるようになった

--- DynamicFrameTest/ViewModels/MainWindowViewModel.cs
+++ DynamicFrameTest/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,9 @@
-﻿using Prism.Mvvm;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using Prism.Mvvm;
 
 namespace DynamicFrameTest.ViewModels
 {
@@ -7,13 +12,22 @@ namespace DynamicFrameTest.ViewModels
         private string _title = "Prism Application";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => this._title;
+            set => this.SetProperty(ref this._title, value);
         }
+        public FlowDocument Document { get; }
 
         public MainWindowViewModel()
         {
-
+            using FileStream stream = File.OpenRead(@"pages/Page1.xaml");
+            this.Document = XamlReader.Load(stream) as FlowDocument;
+            // 動的にevent handlerを指定する
+            (this.Document.FindName("button1") as Button).Click += (s, e) =>
+              {
+                  // 動的にpropertyを読み込む
+                  var textBox = this.Document.FindName("answer1") as TextBox;
+                  _ = MessageBox.Show(textBox.Text);
+              };
         }
     }
 }
